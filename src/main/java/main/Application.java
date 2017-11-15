d1a2827eff7b6042fe1728c3feea75d9285c1d7e
ArkadiuszParafiniuk <arkadiusz.parafiniuk@gmail.com>
2017-11-15 20:58:36 +0100

    Master and Slave services added

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,8 +1,19 @@
 package main;
 
+import org.apache.log4j.Logger;
+
+/**
+ * Class created by Arkadiusz Parafiniuk
+ * arkadiusz.parafiniuk@gmail.com
+ */
 public class Application {
+
+    final static Logger logger = Logger.getLogger(Application.class);
+
     public static void main(String[] args) {
+        logger.info("Application started");
 
 
+        logger.info("Application stopped");
     }
 }
