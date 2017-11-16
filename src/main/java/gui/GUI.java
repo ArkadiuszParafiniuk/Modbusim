7ee2987fdad507c08a5b70c9a8a5e7922f2db8d7
ArkadiuszParafiniuk <arkadiusz.parafiniuk@gmail.com>
2017-11-16 20:08:20 +0100

    #2 GUI Sample

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -4,16 +4,36 @@ package gui;/**
  */
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Welcome!");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
 
+            public void handle(ActionEvent event) {
+                System.out.println("Welcome to modbusim!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 600, 500);
+
+        primaryStage.setTitle("Modbusim!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main() {
+        launch();
     }
 }
